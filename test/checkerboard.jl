2a6bbeb75cbae5ac8f7e3f24b0813661f821ca14
zach <zachshinnic03@outlook.com>
2022-11-24 17:46:36 +1030

    Added passing tests for checkerboard function

--- test/checkerboard.jl
+++ test/checkerboard.jl
@@ -1,7 +1,18 @@
 @testset "Checkerboard" begin
-    # Write you checkerboard test
+    # Check square_size produces correct size of image with an input
     img = checkerboard(10,10 ; square_size = 1)
     @test size(img) == (10,10)
 
-    # Check that we alternate black and white
+    # Check square_size produces correct size of image without an input
+    img = checkerboard(10,10)
+    @test size(img) == (500,500)
+
+    # Check that we alternate black and white vertically
+    img = checkerboard(10,10 ; square_size = 1)
+    @test img[1,1] != img[2,1]
+
+    # Check that we alternate black and white horizontally
+    img = checkerboard(10,10 ; square_size = 1)
+    @test img[1,1] != img[1,2]
+
 end
