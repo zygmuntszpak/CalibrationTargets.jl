6c1089468fd3875b3684cbdb5ea121bd45e9a189
zach <zachshinnic03@outlook.com>
2022-11-24 18:04:28 +1030

    Added test of individual square size allocation

--- test/checkerboard.jl
+++ test/checkerboard.jl
@@ -1,9 +1,13 @@
 @testset "Checkerboard" begin
+    # Check square_size is assigned correctly 
+    img = checkerboard(1,1; square_size =1000)
+    @test size(img) == (1000,1000)
+    
     # Check square_size produces correct size of image with an input
     img = checkerboard(10,10 ; square_size = 1)
     @test size(img) == (10,10)
 
-    # Check square_size produces correct size of image without an input
+    # Check square_size produces correct size of image without an input (deafult square_size == 50)
     img = checkerboard(10,10)
     @test size(img) == (500,500)
 
@@ -15,4 +19,11 @@
     img = checkerboard(10,10 ; square_size = 1)
     @test img[1,1] != img[1,2]
 
+    # Check every second square is assigned white
+    img = checkerboard(2,6)
+    @test img[1,1] == img[1,3] == img[1,5] == img[2,2] == img[2,4] == img[2,6] == 1
+
+    # Check every othere square is assigned black
+    img = checkerboard(2,6)
+    @test img[1,2] == img[1,4] == img[1,6] == img[2,1] == img[2,3] == img[2,5] == 1
 end
