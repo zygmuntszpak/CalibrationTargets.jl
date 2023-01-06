c8c5d6da3e283af86b1b6c1e28e391bfdb08e75a
zach <zachshinnic03@outlook.com>
2023-01-06 17:29:09 +1100

    Adds working detect points fun

--- scripts/runDetectPoints.jl
+++ scripts/runDetectPoints.jl
@@ -9,3 +9,6 @@ using Rotations
 
 img = checkerboard(8,8)
 detect_points(img)
+
+img2 = Gray{Float64}.(load("data/checkerboard_example.jpg"))
+detect_points(img2)
--- src/detect_points.jl
+++ src/detect_points.jl
@@ -1,4 +1,6 @@
 function detect_points(img::AbstractArray)
+    
+    main = Gray{Float64}.(img)
     template_cb = Float64.(checkerboard(2,2 ; square_size = 5)) 
     template_cb = template_cb[1:end-1, 1:end-1]
     window = axes(centered(template_cb))
@@ -9,14 +11,14 @@ function detect_points(img::AbstractArray)
     # trfm = recenter(RotMatrix(deg2rad(rotation_angle)), center(img));
     # imgw = warp(img, trfm);
 
-    out₁ = mapwindow(f₁, img, window)
+    out₁ = mapwindow(f₁, main, window)
     responses = cat(out₁; dims = 3)
     out = pick_maximum_response(responses)
     # Apply the inverse warp to bring the image back to its original pose
     # Fimg = warp(out,inv(trfm));
    
    
-    out = non_max_supress(out, 0.45, 10)
+    out = non_max_supress(out, 0.4, 10)
     imshow(out) 
 end
 
