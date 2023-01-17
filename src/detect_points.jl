0de4de21266a77cf2b266886b0d83b0b5ffb45f7
zach <zachshinnic03@outlook.com>
2023-01-17 16:10:44 +1030

    Adds first implement improving template bank

--- src/detect_points.jl
+++ src/detect_points.jl
@@ -1,38 +1,25 @@
 function detect_points(img::AbstractArray, quality::Number, min_dist::Int )
     
     main = Gray{Float64}.(img)
-   
-   # template_cb = Float64.(checkerboard(2,2 ; square_size = 5)) 
-   # template_cb = template_cb[1:end-1, 1:end-1]
-    
-    #template_cb = construct_sectors() ## Creates 90 degree deafult template
-   
-    template_cb = construct_sectors(0,22.5)    
-    f₁ = Template(template_cb)
-    window = axes(centered(template_cb))
-    out₁ = mapwindow(f₁, main, window)
 
-    template_cb2 = construct_sectors(0,45)    
-    f2 = Template(template_cb2)
-    window2 = axes(centered(template_cb2))
-    out2 = mapwindow(f2, main, window2)
+    angles = deg2rad.([0, 15, 30, 45, 60, 75])
 
-    template_cb3 = construct_sectors(0,67.5)    
-    f3 = Template(template_cb3)
-    window3 = axes(centered(template_cb3))
-    out3 = mapwindow(f3, main, window3)
+    responses = Array{Any}(undef,0,0,0) # need a data structure to store all the responses
 
-    template_cb4 = construct_sectors()    
-    f4 = Template(template_cb4)
-    window4 = axes(centered(template_cb4))
-    out4 = mapwindow(f4, main, window4)
+    for angle in angles
+        template_cb = construct_sectors(0,angle)    
+        f₁ = Template(template_cb)
+        window = axes(centered(template_cb))
+        out = mapwindow(f₁, main, window)
+        push!(out,responses)
+    end
 
-    layerd_responses = cat(out₁,out2,out3,out4; dims = 3);
+    layerd_responses = cat(responses...; dims = 3); # Layer All Together
 
-    out = pick_maximum_response(layerd_responses)
-    # Apply the inverse warp to bring the image back to its original pose
-    # Fimg = warp(out,inv(trfm));
+    out = pick_maximum_response(layerd_responses)   # Take best point for each pixel
+
     non_max_supress!(out, quality, min_dist)
+
     return out
 end
 
