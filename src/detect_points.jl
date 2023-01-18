805c0eaf5f0e8d2869e35125138f21c5fd97950f
zach <zachshinnic03@outlook.com>
2023-01-18 16:20:55 +1030

    Adds squeezes to template bank

--- src/detect_points.jl
+++ src/detect_points.jl
@@ -2,16 +2,18 @@ function detect_points(img::AbstractArray, quality::Number, min_dist::Int )
     
     main = Gray{Float64}.(img)
 
-    angles = deg2rad.([0, 15, 30, 45, 60, 75])
-
+    rotations = [0, 15, 30, 45, 60, 75]
+    squeezes = [0, 9, 18, 27, 36, 45]
     responses = Array{Any}(undef,0,0,0) # need a data structure to store all the responses
 
-    for angle in angles
-        template_cb = construct_sectors(0,angle)    
+    for rotation in rotations
+        for squeeze in squeezes
+        template_cb = construct_sectors(squeeze,rotation)    
         f₁ = Template(template_cb)
         window = axes(centered(template_cb))
         out = mapwindow(f₁, main, window)
         push!(out,responses)
+        end
     end
 
     layerd_responses = cat(responses...; dims = 3); # Layer All Together
