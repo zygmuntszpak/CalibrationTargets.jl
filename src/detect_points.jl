7218868e98217672d328acd8f93c2b842cad2075
zach <zachshinnic03@outlook.com>
2023-01-19 15:53:58 +1030

    Improves template bank

--- src/detect_points.jl
+++ src/detect_points.jl
@@ -1,10 +1,9 @@
 function detect_points(img::AbstractArray, quality::Number, min_dist::Int )
     
     main = Gray{Float64}.(img)
-
     rotations = [0, 15, 30, 45, 60, 75]
-    squeezes = [0, 9, 18, 27, 36, 45]
-    responses = Array{Any}(undef,0,0,0) # need a data structure to store all the responses
+    squeezes = [0, 9, 18, 27, 36]
+    responses = Vector{Matrix{Float64}}()# need a data structure to store all the responses
 
     for rotation in rotations
         for squeeze in squeezes
@@ -12,7 +11,7 @@ function detect_points(img::AbstractArray, quality::Number, min_dist::Int )
         f₁ = Template(template_cb)
         window = axes(centered(template_cb))
         out = mapwindow(f₁, main, window)
-        push!(out,responses)
+        push!(responses,out)
         end
     end
 
